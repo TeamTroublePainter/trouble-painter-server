0a3079d17020c23d32e83735d3ed577de4a9da67
sunwoong <sunwoong1122@gmail.com>
2024-10-21 10:12:13 +0900

    DRAW-415 feat: rt expired time 구분

--- domain/src/main/kotlin/com/xorker/draw/auth/token/RefreshTokenRepository.kt
+++ domain/src/main/kotlin/com/xorker/draw/auth/token/RefreshTokenRepository.kt
@@ -1,11 +1,12 @@
 package com.xorker.draw.auth.token
 
 import com.xorker.draw.user.UserId
+import java.time.temporal.TemporalAmount
 
 interface RefreshTokenRepository {
     fun getUserIdOrThrow(refreshToken: String): UserId
 
-    fun createRefreshToken(userId: UserId): String
+    fun createRefreshToken(userId: UserId, expiredTime: TemporalAmount): String
 
     fun deleteRefreshToken(userId: UserId)
 }
