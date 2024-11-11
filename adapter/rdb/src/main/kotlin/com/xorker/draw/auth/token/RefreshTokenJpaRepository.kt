cdcc19a109c9d62156b22ca539a685a5a3de6553
sunwoong <sunwoong1122@gmail.com>
2024-11-11 16:28:01 +0900

    DRAW-443 feat: 탈퇴 시 소셜 플랫폼 정보 제거 로직 추가

--- adapter/rdb/src/main/kotlin/com/xorker/draw/auth/token/RefreshTokenJpaRepository.kt
+++ adapter/rdb/src/main/kotlin/com/xorker/draw/auth/token/RefreshTokenJpaRepository.kt
@@ -1,7 +1,9 @@
 package com.xorker.draw.auth.token
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 
 internal interface RefreshTokenJpaRepository : JpaRepository<RefreshTokenJpaEntity, ByteArray> {
+    @Modifying
     fun deleteByUserId(userId: Long)
 }
